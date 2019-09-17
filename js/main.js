5fd5902400a379e3fa117062b09b92f13c9f6630
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2019-09-16 23:41:09 -0300

    Agregando mas audios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,44 @@
+//Tomate y lechuga
+
 $(document).ready(function() {
 	$('#tyl').click(function(){
 		$('audio')[0].play(	);
+	});
+
+});
+
+//Y BOLUDO
+
+$(document).ready(function() {
+	$('#yboludo').click(function(){
+		$('audio')[1].play(	);
+	});
+
+});
+
+//Escribi bien hdp
+
+$(document).ready(function() {
+	$('#escribiBn').click(function(){
+		$('audio')[2].play(	);
+	});
+
+});
+
+//Banca
+
+$(document).ready(function() {
+	$('#banca').click(function(){
+		$('audio')[3].play(	);
+	});
+
+});
+
+//Escribi bien hdp
+
+$(document).ready(function() {
+	$('#dameBola').click(function(){
+		$('audio')[4].play(	);
 	});
 
 });
