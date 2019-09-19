e30e28d0592f529221594ca0ee9fac8927e5c96a
leonelgwetzel <wetzel.leonel.gabriel@gmail.com>
2019-09-18 21:58:31 -0300

    Agregando mas audios

--- js/main.js
+++ js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
 
 });
 
-//Escribi bien hdp
+//Dame bola hdp
 
 $(document).ready(function() {
 	$('#dameBola').click(function(){
@@ -42,3 +42,95 @@ $(document).ready(function() {
 	});
 
 });
+
+//Chupenla
+$(document).ready(function() {
+	$('#chupenla').click(function(){
+		$('audio')[5].play(	);
+	});
+
+});
+
+//como te fue
+$(document).ready(function() {
+	$('#cmotefue').click(function(){
+		$('audio')[6].play(	);
+	});
+
+});
+
+//como te fue
+$(document).ready(function() {
+	$('#contestaputo').click(function(){
+		$('audio')[7].play(	);
+	});
+
+});
+
+//dejate de romper las bolas
+$(document).ready(function() {
+	$('#dejadejoder').click(function(){
+		$('audio')[8].play(	);
+	});
+
+});
+
+//dejate de romper las bolas
+$(document).ready(function() {
+	$('#drama').click(function(){
+		$('audio')[9].play(	);
+	});
+
+});
+
+//No podes mandar audios
+$(document).ready(function() {
+	$('#mandaraudios').click(function(){
+		$('audio')[10].play( );
+	});
+
+});
+
+//Noticias viejas
+$(document).ready(function() {
+	$('#noticiasviejas').click(function(){
+		$('audio')[11].play( );
+	});
+
+});
+
+//O sos pelotudo
+$(document).ready(function() {
+	$('#osospelotudo').click(function(){
+		$('audio')[12].play( );
+	});
+
+});
+
+//Que que decis
+$(document).ready(function() {
+	$('#quedecis').click(function(){
+		$('audio')[13].play( );
+	});
+
+});
+
+//Vas a venir mañana?
+$(document).ready(function() {
+	$('#vasavenir').click(function(){
+		$('audio')[14].play( );
+	});
+
+});
+
+//no te hagas la victima
+$(document).ready(function() {
+	$('#victima').click(function(){
+		$('audio')[15].play( );
+	});
+
+});
+
+
+
+
